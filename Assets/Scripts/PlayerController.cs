87298fedecac9755d7c25554d39018879f50d538
Rory O'Mahony <rory1510@hotmail.co.uk>
2018-04-14 20:21:54 +0100

    Added basic movement and rotation to the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float movementSpeed;
+	public float rotationSpeed;
+
+	private Rigidbody2D player;
+
 	// Use this for initialization
-	// Danny comment!!!
 	void Start () {
-		
+		player = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	// Use for physics calculations
+	void FixedUpdate(){
+		applyPlayerRotation();
+		applyPlayerMovement();
+	}
+
+	private void applyPlayerRotation(){
+		float playerRotation = calculateHorizontalRotation();
+		player.AddTorque(playerRotation);
+	}
+
+	/* 
+		Calculates the horizontal rotation of the player. Note that this flips the horizontal input so 
+	  	that the player is rotated based on the key that they press, as Unity uses the "Left-Hand Coordinate System".
+	*/
+	private float calculateHorizontalRotation(){
+		return -Input.GetAxis("Horizontal") * rotationSpeed;
+	}
+
+	private void applyPlayerMovement(){
+		float verticalMovement = Input.GetAxis("Vertical");
+		Vector2 playerMovement = new Vector2(0, verticalMovement);
+		player.AddRelativeForce(playerMovement * movementSpeed);
+	}
+	
 }
