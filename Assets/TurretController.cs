27dbea728f08cfcae6dc13c132af7de089599c54
Danny <danj15@hotmail.com>
2018-04-27 15:43:23 +0100

    turret code refactor and some render order changes for the turrets

--- Assets/TurretController.cs
+++ Assets/TurretController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour {
-	public float offset = -90.0f;
+	//used to calibrate the rotation, such that the top of the sprite (the barrel of the gun) is the point that aims at the mouse, by default it's the side of the sprite
+	public float turretSpriteOffset = -90.0f;
+	//the parent ship of this turret
 	public GameObject ship;
 
 	// Use this for initialization
@@ -13,9 +15,21 @@ public class TurretController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		aimTurrets ();
+	}
+
+	void aimTurrets () {
+		float rotationZ = getAimAngle ();
+		transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + turretSpriteOffset);
+	}
+
+	float getAimAngle (){
+		/*Gets the rotation for the turrets to aim, we dont actually want the turrets to aim at the mouse, we want them to aim along a line
+		parallel with the line between the centre of the ship and the mouse, so that the bullets all fire parallel, so we take the parent ship
+		position to compare with the mouse position, we then normalise so that our rotation vector is simply a direction with no magnitude */
 		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - ship.transform.position;
 		difference.Normalize();
-		float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+		//turn our direction vector into an angle to rotate in degrees
+		return Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 	}
 }
