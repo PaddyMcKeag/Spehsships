9bf9d464f7f233c3262db14e10b0de112a5f9a2a
Rory O'Mahony <rory1510@hotmail.co.uk>
2018-04-29 15:10:26 +0100

    Create a script for the camera that allows it to track an object.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject objectToTrack;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - objectToTrack.transform.position;
+	}
+	
+	// Runs after all code in 'Update' has been executed
+	void LateUpdate () {
+		positionCameraAtTrackedObject();
+	}
+
+	private void positionCameraAtTrackedObject(){
+		transform.position = objectToTrack.transform.position + offset;
+	}
+}
