e2fbc4e69e22c289b24e8e0d638f0196f4cb8b5b
Danny <danj15@hotmail.com>
2018-05-12 15:54:12 +0100

    bullet explosions

---
+++ Assets/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+
+	//explosion on hit
+	//premade bullet prefab ready to spawn
+	public GameObject boomPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D col) {
+		Destroy (this.gameObject);
+		explode ();
+	}
+
+	void explode(){
+		var explosion = (GameObject)Instantiate (
+			boomPrefab,
+			this.transform.position,
+			this.transform.rotation);
+		Destroy(explosion, 0.1f);
+	}
+		
+}
